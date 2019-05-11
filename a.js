8ae6ca5664415d898e412fcde0c13cc8ec88fada
yutooni <yuto.oni1227@gmail.com>
2019-05-11 19:00:56 +0900

    ホラーな機能を追加

--- a.js
+++ a.js
@@ -1,1 +1,11 @@
 console.log("すごい機能")
+const button = document.getElementById('button1')
+const div1 = document.getElementById('div1')
+
+const changeColor = () =>{
+    console.dir(div1);
+    div1.innerHTML = "よくも押したなー。"
+}
+button.addEventListener('click',()=>{
+    changeColor()
+})
